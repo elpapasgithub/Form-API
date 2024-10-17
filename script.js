2f191b088a29f91501e83ed07ea63ca0a6699b47
el pepas <83471467+elpapasgithub@users.noreply.github.com>
2024-10-17 17:34:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+class User {
+    constructor(username, password, date) {
+        this.username = username;
+        this.password = password;
+        this.date = date;
+    }
+}
+
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    const date = document.getElementById('date').value;
 
-    const newUser = new User(username, password, date);
+    const newUser = new User(username, password);
 
     const jsonUser = JSON.stringify(newUser);
 
